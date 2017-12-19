f171ee590b3531beb03f9f5eae28b6b60a272e30
RemiLecouillard <remizbay@hotmail.fr>
2017-12-19 18:05:17 +0100

    add algo

---
+++ include/fct/algo.h
@@ -0,0 +1,20 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+/**
+  * @file algo.h
+  */
+#ifndef _ALGO_H
+#define _ALGO_H
+
+#include "fct/rag.h"
+
+void segmentateRegion(RAG rag);
+
+#endif
---
+++ src/fct/algo.c
@@ -0,0 +1,13 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+/**
+  * @file algo.c
+  */
+#include "list/algo.h"
