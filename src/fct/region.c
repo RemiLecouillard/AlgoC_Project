a5e3282c202ff1f6b236489130c51109b1eff3cd
RemiLecouillard <remizbay@hotmail.fr>
2017-12-20 15:46:47 +0100

    Remove useless stuffs

--- src/fct/region.c
+++ src/fct/region.c
@@ -73,7 +73,6 @@ void fusion(Region reg1,Region reg2){
 
   res->moments = mergeMoments(reg1->region->moments, reg2->region->moments);
   res->neighbours = mergeList(reg1->region->neighbours, reg2->region->neighbours);
-  res->quadraticError = getQE(res->moments);
 
   destroyStructRegion(reg1->region);
   destroyStructRegion(reg2->region);
